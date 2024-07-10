85fec5bf6ff0d94cb69306406ccbc0432a882d34
罗拉 <me@luola.me>
2024-07-10 20:56:05 +0800

    feat: Add. env configuration file function to read

---
+++ core/GetConfig.go
@@ -0,0 +1,16 @@
+package core
+
+import (
+	_ "github.com/joho/godotenv/autoload"
+	"github.com/luolayo/gin-study/Config"
+	"os"
+)
+
+func GetSystemConfig() Config.System {
+	return Config.System{
+		AppName:     os.Getenv("APP_NAME"),
+		AppVersion:  os.Getenv("APP_VERSION"),
+		Port:        os.Getenv("PORT"),
+		Environment: os.Getenv("ENVIRONMENT"),
+	}
+}
