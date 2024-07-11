d8b97c6d0953f1f4a63732ba7b91ef8e7eef3b96
罗拉 <me@luola.me>
2024-07-11 17:07:26 +0800

    router: Add Swagger Route

--- Router/Router.go
+++ Router/Router.go
@@ -1,9 +1,14 @@
 package Router
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+)
 
 func GetRouter() *gin.Engine {
 	r := gin.Default()
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	router := r.Group("/test")
 	TestRoutes(router)
 	router = r.Group("/user")
