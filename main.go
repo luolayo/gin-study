0b8531f04dc9107db6bb778421938445dd5a7c9a
罗拉 <me@luola.me>
2024-07-11 17:06:06 +0800

    feat: Add Swagger library and add judgment on the running environment

--- main.go
+++ main.go
@@ -1,15 +1,27 @@
 package main
 
 import (
+	"github.com/gin-gonic/gin"
 	"github.com/luolayo/gin-study/Core"
 	"github.com/luolayo/gin-study/Logger"
 	"github.com/luolayo/gin-study/Router"
+	"github.com/luolayo/gin-study/docs"
 )
 
+// @BasePath /
+// @title Gin Study API
+// @Version 1.0
+// @Description Gin study is a small project for beginners to learn by writing a blog built by the gin framework.
+// @Host localhost:8080
+// @Schemes http https
 func main() {
 	logger := Logger.NewLogger(Logger.InfoLevel)
 	system := Core.GetSystemConfig()
 	Core.PrintSystemInfo(logger, system)
+	if system.Environment == "release" {
+		gin.SetMode(gin.ReleaseMode)
+	}
+	docs.SwaggerInfo.Version = "1.0"
 	router := Router.GetRouter()
 	err := router.Run(":" + system.Port)
 	if err != nil {
